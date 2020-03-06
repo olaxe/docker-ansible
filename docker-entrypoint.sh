968ec04c16aac9d6cf78c35e734aa87fee4a988b
mansionis-dev <54140090+mansionis-dev@users.noreply.github.com>
2020-03-06 14:36:38 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,4 +18,18 @@ else
   echo 'The SSH key is already present so no need to build it'
 fi
 echo ''
+
+echo 'Ensure permissions are good for the root SSH folder'
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/*.*
+echo ''
+
+echo 'Recreate the Ansible configuration folder'
+rm -rf /etc/ansible
+mkdir /etc/ansible
+
+echo 'Clone configuration files from the Git Repository'
+git clone $ANSIBLE_CONFIG_GIT_URL /etc/ansible
+
+echo 'infinite waiting so the container can be used at any time to launch Ansible operations'
 while true; do sleep 10; done
